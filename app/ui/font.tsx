476bc29fcd454d9fee6b5d1bb8cc4960d43c57d2
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-12-11 13:36:52 +0100

    Initial commit ```

---
+++ app/ui/font.tsx
@@ -0,0 +1,6 @@
+import { Engagement, Gabriela, Amiko } from 'next/font/google';
+
+export const engagment = Engagement({ weight: '400', subsets: ['latin'] })
+export const gabriela = Gabriela({ weight: '400',subsets: ['latin'] })
+export const amiko = Amiko({ weight: ['400', '600'], subsets: ['latin'] })
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        crema: '#F3EBE2'
+      },
+      colors: {
+        crema: '#F3EBE2',
+        white: '#FFFFFF',
+        moggle: '#312920',
+        beaver: '#8C7D69',
+        lion: '#BB9A77'
       },
     },
   },
