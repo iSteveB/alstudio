837db1a934ebaf6ab6fbbcd6aaa1061a17c3b52f
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-01-22 20:34:24 +0100

    fix modal props

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -16,7 +16,7 @@ const Modal = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='fixed left-1/2 top-1/2 z-50 h-screen w-full -translate-x-1/2 -translate-y-1/2 bg-black/90'>
             <div className='m-6'>
-                <Button type='submit' text='back' handleRoute={handleBack} />
+                <Button type='submit' text='back' handleClick={handleBack} />
             </div>
 
             <div className='fixed left-1/2 top-1/2 h-auto max-h-screen -translate-x-1/2 -translate-y-1/2 overflow-scroll border-3 border-black shadow-xl shadow-black'>
