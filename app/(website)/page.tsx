d2002033cbfd3601c66e91d677025d5239467a1e
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-01-29 16:17:20 +0100

    Update page.tsx with new image and handle click event

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,7 +1,9 @@
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import pic from '@/public/images/DSC_0066.webp';
-import pic2 from '@/public/images/DSC_0123.webp';
+import pic2 from '@/public/images/MTXX_MR20231229_184426658.webp';
 import Section from '../components/Section';
 import Aside from '../components/Aside';
 
@@ -10,6 +12,7 @@ import { CATEGORIES } from '../data/categories';
 import ImageSectionContainer from '../components/ImageSectionContainer';
 
 const Home = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <main>
             <Section
@@ -60,12 +63,49 @@ const Home = () => {
                     })}
                 </div>
             </section>
+            <Aside />
             <Section
                 title="Derrière l'objectif"
-                subtitle='Une photographe passionnée.'
+                subtitle={isOpen ? undefined : 'Une photographe passionnée.'}
                 buttonText='Me découvrir'
-                buttonLink='/contact'
-                reverse={true}>
+                handleClick={() => setIsOpen(!isOpen)}
+                reverse={true}
+                text={
+                    isOpen && (
+                        <div className='flex flex-col gap-2'>
+                            <p className='text-lg text-moggle'>
+                                Hello, moi c&apos;est Déborah, maman de deux
+                                petits garçons.
+                            </p>
+                            <p className='text-lg text-moggle'>
+                                La photographie est une véritable passion pour
+                                moi, et je suis également très manuelle depuis
+                                mon enfance. <br /> Depuis toujours, je suis une
+                                personne polyvalente, pratiquant le dessin, la
+                                peinture, la couture, et bien d&apos;autres
+                                domaines créatifs.
+                                <br />
+                                Pourquoi la photo? En tant que maman louve,
+                                j&apos;apprécie capturer ces moments hors du
+                                temps. Quoi de plus beau que de mettre ces
+                                instants sur pause, que ce soit lors de
+                                cérémonies, de grossesses ou de naissances?{' '}
+                                <br /> La photographie va bien au-delà de la
+                                simple capture d&apos;images, elle me permet de
+                                venir à votre rencontre et de partager un bout
+                                de votre parcours de vie.
+                            </p>
+                            <p className='text-lg text-moggle'>
+                                Je serai ravie de vous rencontrer et de vous
+                                accompagner dans vos projets. Et n&apos;oubliez
+                                pas, la beauté est dans les yeux de celui qui
+                                regarde....
+                            </p>
+
+                            <p>Déborah.</p>
+                        </div>
+                    )
+                }>
                 <ImageSectionContainer
                     imgSrc={pic2}
                     imgAlt='Déborah Tétéa'
