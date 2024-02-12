c94275103f874298d975d206b2b98eefbf2c8d63
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-02-12 23:06:06 +0100

    fix modal loader

--- app/(website)/portfolio/loading.tsx
+++ app/(website)/portfolio/loading.tsx
@@ -3,7 +3,7 @@ import Loader from '@/app/ui/Loader';
 export default function Loading() {
     return (
         // eslint-disable-next-line tailwindcss/enforces-shorthand
-        <div className='m-auto flex h-screen w-screen items-center justify-center'>
+        <div className='fixed left-1/2 top-1/2 z-50 h-screen w-full -translate-x-1/2 -translate-y-1/2 bg-black/70 backdrop-blur'>
             <Loader />
         </div>
     );
--- app/ui/Loader.tsx
+++ app/ui/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader = () => {
     return (
         <div className='flex size-full items-center justify-center'>
-            <i className='relative grid w-1/2 place-items-center before:absolute before:left-[calc(50%_-_3vmin)] before:top-[calc(50%_-_3vmin)] before:box-border before:size-[3vmin] before:animate-loader-before before:bg-moggle after:absolute after:left-1/2 after:top-1/2 after:box-border after:size-[3vmin] after:animate-loader-after after:bg-moggle'></i>
+            <i className='relative grid w-1/2 place-items-center before:absolute before:left-[calc(50%_-_3vmin)] before:top-[calc(50%_-_3vmin)] before:box-border before:size-[3vmin] before:animate-loader-before before:bg-crema after:absolute after:left-1/2 after:top-1/2 after:box-border after:size-[3vmin] after:animate-loader-after after:bg-crema'></i>
         </div>
     );
 };
