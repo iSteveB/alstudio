8f7567f988df9e6b7809f8eb3762dbfc175f19ca
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-02-14 09:28:00 +0100

    fix links props

--- app/data/links.tsx
+++ app/data/links.tsx
@@ -4,19 +4,19 @@ import Email from '../ui/svg/Email';
 export const links = [
     {
         href: 'https://www.facebook.com/rehaussementdecilsbydeborah',
-        icon: <Facebook color='#8B7E69'/>,
+        icon: <Facebook color={1}/>,
         text: 'Facebook',
         newTab: true
     },
     {
         href: 'https://instagram.com/alstudiophoto?igshid=NGVhN2U2NjQ0Yg==',
-        icon: <Instagram color='#8B7E69'/>,
+        icon: <Instagram color={1}/>,
         text: 'Instagram',
         newTab: true
     },
     {
         href: 'https://www.tiktok.com/@a.l.studiophotos?_t=8gS9kIGYT8C&_r=1',
-        icon: <Tiktok color='#8B7E69'/>,
+        icon: <Tiktok color={1}/>,
         text: 'TikTok',
         newTab: true
     },
